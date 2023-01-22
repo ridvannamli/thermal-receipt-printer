f634d2366face59eda48b7299d3f3cbd24dbc010
ridvannamli <ridvannamli@gmail.com>
2023-01-22 16:48:56 +0300

    Thermal Receipt Printer Operations
    Thermal Receipt Printer Operations in C#

---
+++ ThermalPrinterOperations/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace ThermalPrinterOperations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnPrintReceipt = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtCareer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFirmName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPrintReceipt
+            // 
+            this.btnPrintReceipt.Location = new System.Drawing.Point(200, 152);
+            this.btnPrintReceipt.Name = "btnPrintReceipt";
+            this.btnPrintReceipt.Size = new System.Drawing.Size(105, 37);
+            this.btnPrintReceipt.TabIndex = 28;
+            this.btnPrintReceipt.Text = "Print Receipt";
+            this.btnPrintReceipt.UseVisualStyleBackColor = true;
+            this.btnPrintReceipt.Click += new System.EventHandler(this.btnPrintReceipt_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(127, 51);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 21;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(127, 77);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 22;
+            // 
+            // txtCareer
+            // 
+            this.txtCareer.Location = new System.Drawing.Point(127, 103);
+            this.txtCareer.Name = "txtCareer";
+            this.txtCareer.Size = new System.Drawing.Size(100, 20);
+            this.txtCareer.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Career";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Surname";
+            // 
+            // txtFirmName
+            // 
+            this.txtFirmName.Location = new System.Drawing.Point(127, 25);
+            this.txtFirmName.Name = "txtFirmName";
+            this.txtFirmName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirmName.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Firm Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(259, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Product Name";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(367, 25);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(100, 20);
+            this.txtProductName.TabIndex = 24;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(303, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Price";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(303, 106);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Total";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(291, 51);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Amount";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(367, 103);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 27;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(367, 77);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 26;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(367, 51);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtAmount.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 216);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFirmName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCareer);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnPrintReceipt);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(510, 255);
+            this.MinimumSize = new System.Drawing.Size(510, 255);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thermal Printer Operations";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrintReceipt;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtCareer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFirmName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtAmount;
+    }
+}
+
---
+++ ThermalPrinterOperations/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThermalPrinterOperations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        PrintDocument pd = new PrintDocument();
+
+
+        public void PrintReceipt()
+        {
+            try
+            {
+                pd.PrintPage += Pd_PrintPage;
+                pd.Print();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Pd_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            // if thermal printer's width 58 mm we must set to maximum 220 pixel in program 
+
+            if (txtFirmName.Text != null && txtFirmName.Text != "" && txtName.Text != null && txtName.Text != "" && txtSurname.Text != null && txtSurname.Text != "" && txtCareer.Text != null && txtCareer.Text != "" && txtProductName.Text != null && txtProductName.Text != "" && txtAmount.Text != null && txtAmount.Text != "" && txtPrice.Text != null && txtPrice.Text != "" && txtTotal.Text != null && txtTotal.Text != "")
+            {
+                int paperHeight = 120;
+                PaperSize ps58 = new PaperSize("58mm Thermal", 220, paperHeight + 120);
+                pd.DefaultPageSettings.PaperSize = ps58;
+
+                Font fontHeader = new Font("Calibri", 10, FontStyle.Bold);
+                Font fontInfo = new Font("Calibri", 8, FontStyle.Bold);
+                Font fontcontentHeader = new Font("Calibri", 8, FontStyle.Underline);
+                StringFormat center = new StringFormat(StringFormatFlags.FitBlackBox);
+                center.Alignment = StringAlignment.Center;
+                RectangleF rcFirmNameLocation = new RectangleF(0, 20, 220, 20); // (int x, int y, float width, float height)
+                // e.Graphics.DrawString("abc", fontHeader, Brushes.Black, new Point(5, 5));
+                e.Graphics.DrawString(txtFirmName.Text, fontHeader, Brushes.Black, rcFirmNameLocation, center);
+                e.Graphics.DrawString("Name       : " + txtName.Text, fontInfo, Brushes.Black, new Point(5, 45));
+                e.Graphics.DrawString("Surname : " + txtSurname.Text, fontInfo, Brushes.Black, new Point(5, 60));
+                e.Graphics.DrawString("Career      : " + txtCareer.Text, fontInfo, Brushes.Black, new Point(5, 75));
+                e.Graphics.DrawString("------------------------------------------------------------", fontInfo, Brushes.Black, new Point(5, 90));
+
+                e.Graphics.DrawString("Product 1", fontcontentHeader, Brushes.Black, new Point(5, 105));
+                e.Graphics.DrawString("Amount", fontcontentHeader, Brushes.Black, new Point(100, 105));
+                e.Graphics.DrawString("Price", fontcontentHeader, Brushes.Black, new Point(140, 105));
+                e.Graphics.DrawString("Total", fontcontentHeader, Brushes.Black, new Point(180, 105));
+                              
+
+                e.Graphics.DrawString(txtProductName.Text, fontInfo, Brushes.Black, new Point(5, paperHeight));
+                e.Graphics.DrawString(txtAmount.Text, fontInfo, Brushes.Black, new Point(115, paperHeight));
+                e.Graphics.DrawString(Convert.ToDouble(txtPrice.Text).ToString("C2"), fontInfo, Brushes.Black, new Point(140, paperHeight));
+                e.Graphics.DrawString(Convert.ToDouble(txtTotal.Text).ToString("C2"), fontInfo, Brushes.Black, new Point(180, paperHeight));  // ("C2") -> Money Format -> ₺ 
+                paperHeight += 15;
+
+                e.Graphics.DrawString("------------------------------------------------------------", fontInfo, Brushes.Black, new Point(5, paperHeight));
+                e.Graphics.DrawString("TOTAL : " + Convert.ToDouble(txtTotal.Text).ToString("C2"), fontHeader, Brushes.Black, new Point(5, paperHeight + 20));
+                e.Graphics.DrawString("------------------------------------------------------------", fontInfo, Brushes.Black, new Point(5, paperHeight + 40));
+                e.Graphics.DrawString("(Has No Financial Value)", fontInfo, Brushes.Black, new Point(5, paperHeight + 60));
+                // Some of the Thermal Printers also have a cutting process. Codes can be added.                
+            }
+        }
+
+        private void btnPrintReceipt_Click(object sender, EventArgs e)
+        {
+            PrintReceipt();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
